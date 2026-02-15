67c5693ac7cba71e405304f1f1c434dd31002a1d
Puneet Kumar Singh <punitku624@gmail.com>
2026-02-15 22:05:32 +0530

    fix github action db connection

--- scraper/db.py
+++ scraper/db.py
@@ -2,5 +2,5 @@ import os
 import psycopg2
 
 def get_connection():
-    db_url = os.environ.get("DATABASE_URL")
+    db_url = os.environ["DATABASE_URL"]   # must be this exact line
     return psycopg2.connect(db_url)
